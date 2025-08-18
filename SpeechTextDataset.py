d9de495665549dc2ffa4631f519819bb697377a6
Süleyman Poyraz Köroğlu <poyraz.koroglu@sestek.com>
2025-08-18 11:40:18 +0300

    The data is now padded by repition or trunctuated according to its length, transcript implementation to be added.

--- SpeechTextDataset.py
+++ SpeechTextDataset.py
@@ -17,11 +17,11 @@ class SpeechTextDataset(Dataset):
         self.transform = transform
         self.excel_path = excel_path
 
-        unique_labels = sorted(self.df["Label"].unique())
+        labels = sorted(self.df["Label"].unique())
         self.label_map = {}
-        if "neutral" in unique_labels:
+        if "neutral" in labels:
             self.label_map["neutral"] = 0
-        if "negative" in unique_labels:
+        if "negative" in labels:
             self.label_map["negative"] = 1
 
 
@@ -31,18 +31,20 @@ class SpeechTextDataset(Dataset):
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
 
-        audio_np, sr = librosa.load(row["Audio"], sr=self.sample_rate)  # sr=self.sample_rate resamples automatically
+        audio_np, sr = librosa.load(row["Audio"], sr=self.sample_rate)
         audio_tensor = torch.tensor(audio_np).unsqueeze(0)
-        length_seconds = audio_tensor.shape[1] / sr
         target_seconds = 4
         target_samples = int(self.sample_rate * target_seconds)
+        n_samples = audio_np.shape[0].shape[1]
 
-        if audio_tensor.shape[1] > target_samples:
+
+        if n_samples < target_samples:
+            repeats = target_samples // n_samples
+            remainder = target_samples % n_samples
+            audio_tensor = audio_tensor.repeat(1, repeats)  # repeat along time axis
+            audio_tensor = torch.cat([audio_tensor, audio_tensor[:, :remainder]], dim=1)
+        elif n_samples > target_samples:
             audio_tensor = audio_tensor[:, :target_samples]
-        elif audio_tensor.shape[1] < target_samples:
-            padding = target_samples - audio_tensor.shape[1]
-            audio_tensor = torch.nn.functional.pad(audio_tensor, (0, padding))
-
 
         if self.transform:
             audio_tensor = self.transform(audio_tensor)
